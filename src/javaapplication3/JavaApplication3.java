15b69b3c4fce896602ca4b5ccd43709ca99386c3
ALUNO <ALUNO@192.168.163.26>
2018-09-19 20:31:10 -0300

    commit fazer commit Hello Git Fazer Commit

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -16,6 +16,10 @@ public class JavaApplication3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello Git");
+        
+        
     }
     
 }
